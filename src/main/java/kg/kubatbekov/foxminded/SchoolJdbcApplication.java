f15eb599adcbb5975bd1cf00901f5b8cc9e1f20e
Kubatbekov Nurlan <kubatbekovnurlan13@gmail.com>
2023-02-09 19:09:36 +0600

    First commit: spring boot initialized

---
+++ src/main/java/kg/kubatbekov/foxminded/SchoolJdbcApplication.java
@@ -0,0 +1,13 @@
+package kg.kubatbekov.foxminded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolJdbcApplication.class, args);
+	}
+
+}
